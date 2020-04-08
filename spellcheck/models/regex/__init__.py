4e4c2d8feccf657facdecdcf005516672e66ce49
Wauplin <lucainp@gmail.com>
2020-04-08 15:17:36 +0200

    Replacement patterns in external file + flashtext

--- spellcheck/models/regex/__init__.py
+++ spellcheck/models/regex/__init__.py
@@ -1,23 +1,35 @@
+from pathlib import Path
 from regex import format_percentages
+from flashtext import KeywordProcessor
 
 from models.base import BaseModel
 
+# TODO : shall be a paraameter
+patterns_path = Path(__file__).parent / 'patterns_fr.txt'
+
 
 class RegexModel(BaseModel):
-    REPLACE_LIST = {
-        '&quot;': '*',
-        'oeu': 'œu',
-        'OEU': 'ŒU',
-        'OEu': 'Œu',
-        'Oeu': 'Œu',
-    }
-
     def __init__(self, only_option=None):
         super(RegexModel, self).__init__()
         self.only_option = only_option
+        self.keyword_processor = self._load_keywords()
+
+    def _load_keywords(self):
+        keyword_processor = KeywordProcessor(case_sensitive=True)
+        with patterns_path.open() as f:
+            current_replacement = None
+            for line in f.readlines():
+                line = line.strip()
+                if line.startswith('#') or len(line) == 0:
+                    current_replacement = None
+                elif current_replacement is None:
+                    current_replacement = line
+                else:
+                    keyword_processor.add_keyword(line, current_replacement)
+        return keyword_processor
 
     def predict(self, items):
-        return [self.apply_regex(item['original']) for item in items]
+        return [self.apply(item['original']) for item in items]
 
     @property
     def name(self):
@@ -26,7 +38,7 @@ class RegexModel(BaseModel):
         else:
             return f'RegexModel ({self.only_option})'
 
-    def apply_regex(self, txt):
+    def apply(self, txt):
         if self.only_option is None:
             txt = self.apply_percentages(txt)
             txt = self.apply_replacements(txt)
@@ -40,6 +52,4 @@ class RegexModel(BaseModel):
         return format_percentages(txt)
 
     def apply_replacements(self, txt):
-        for replace_key, replace_value in self.REPLACE_LIST.items():
-            txt = txt.replace(replace_key, replace_value)
-        return txt
+        return self.keyword_processor.replace_keywords(txt)
