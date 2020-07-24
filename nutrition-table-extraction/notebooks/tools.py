49d31ee8c49319934393e06a30ad46902a0c19ae
Ramzi Missaoui <ramzi.missaoui@wavestone.com>
2020-07-24 10:49:40 +0200

    manual approach for line detecting based on bounding box coordinates

--- nutrition-table-extraction/notebooks/tools.py
+++ nutrition-table-extraction/notebooks/tools.py
@@ -8,6 +8,7 @@ from sys import exit
 from requests import get, post
 
 import cv2
+import numpy as np
 import filetype
 import matplotlib
 import matplotlib.image as mpimg
@@ -139,6 +140,64 @@ def test_single_img(img_id, base_path, endpoint, apim_key, post_url, headers, cr
     return df, json_data
 
 
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
+
+def get_bb_mask(source, json_data, angle_degree):
+    contours_list = []
+    lines = json_data['analyzeResult']['readResults'][0]['lines']
+    for line in lines:
+        contours_list.append(np.array(line["boundingBox"]).reshape(4,2))
+    image = cv2.imread(source, cv2.IMREAD_GRAYSCALE)
+    print(image.shape)
+    image[:,:] = 1
+    image = rotate_image(image, angle_degree)
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    for contour in contours_list:
+        cv2.fillPoly(mask, pts=[contour], color=(255,255,255))
+    masked_image = cv2.bitwise_and(image, mask)
+    masked_image = cv2.equalizeHist(masked_image)
+    return masked_image
+
+def scan_hist(mask, deg=0):
+    pass
 
 
+def iou(y0, y3, b0, b3):
+    u = min(max(y0, b0) - min(y3, b3), y0 - y3 + b0 - b3 )
+    i = max(min(y0, b0) - max(y3, b3), 0 )
+    return i / u
 
+def get_iou(json_data, threshold=0.2):
+    lines = json_data['analyzeResult']['readResults'][0]['lines']
+    angle = json_data['analyzeResult']['readResults'][0]
+    mapping_bb = {}
+    mapping_iou = {i:[] for i in range(len(lines))}
+    for (i, line) in enumerate(lines):
+        bb = np.array(line["boundingBox"])
+        mapping_bb[i] = bb
+        x0, y0 = bb[0], bb[1]
+        x1, y1 = bb[2], bb[3]
+        x2, y2 = bb[4], bb[5]
+        x3, y3 = bb[6], bb[7]
+        for (j, line2) in enumerate(lines):
+            if j == i:
+                continue
+            bb2 = np.array(line2["boundingBox"])
+            a0, b0 = bb2[0], bb2[1]
+            a1, b1 = bb2[2], bb2[3]
+            a2, b2 = bb2[4], bb2[5]
+            a3, b3 = bb2[6], bb2[7]
+            iou_value = iou(y3, y0, b3, b0)
+            print(i, j)
+            print(y0, y3, b0, b3)
+            print(iou_value)
+            print("--"*20)
+            if iou_value > threshold:
+                mapping_iou[i].append({'id': j, 'iou_value': iou_value})
+    print(len(lines))
+    #return [(mapping_bb[i], mapping_bb[j['id']]) for j in mapping_iou[i] for i in mapping_iou.keys()]
+    return mapping_iou
