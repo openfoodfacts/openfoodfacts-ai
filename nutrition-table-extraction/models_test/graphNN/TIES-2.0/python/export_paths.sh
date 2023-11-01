a91560ff689dbd4858006d5750e641a4562cacfc
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-11-01 15:18:44 +0100

    remove nutrition table extraction data
    it can still be access through a specific branch.
    The idea is to unbloat the repo to allow fast shallow git repo clone.

--- nutrition-table-extraction/models_test/graphNN/TIES-2.0/python/export_paths.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/home/ramzimissaoui/missions/off/local/models_test/caloGraphNN
-export PYTHONPATH=$PYTHONPATH:/home/ramzimissaoui/missions/off/local/models_test/TIES-2.0/python
