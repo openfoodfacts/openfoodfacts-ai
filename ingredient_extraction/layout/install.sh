05b30749c407db5bb63d4ab551349f564974cda1
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-11-01 15:42:58 +0100

    layoutlm: add install script

---
+++ ingredient_extraction/layout/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# You can download and launch this script in one-line with:
+# wget https://raw.githubusercontent.com/openfoodfacts/openfoodfacts-ai/develop/ingredient_extraction/layout/install.sh -O - | bash
+
+# Perform a blobless clone of the repository, it's the fastest way to download
+# the training code (~2.5MB only)
+git clone --filter=blob:none https://github.com/openfoodfacts/openfoodfacts-ai
+
+cd openfoodfacts-ai/ingredient_extraction/layout
+
+python3 -m pip install -r requirements.txt
+
+echo "Install completed!"
