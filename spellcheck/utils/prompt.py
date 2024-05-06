18cae3c3e50e418ad6231cf07b404a6948e91721
JeremyArancio <jeremyarancio@gmail.com>
2024-05-06 12:33:40 +0200

    feat(spellcheck): :arrow_up: Prompt v3

--- spellcheck/utils/prompt.py
+++ spellcheck/utils/prompt.py
@@ -10,46 +10,42 @@ of ingredients in different languages extracted from product packages using Opti
 extract the ingredients from this list using our algorithms. However, it is possible some typos or \
 errors slipped into the list. Your task is to correct those errors following a guideline I provide you.
 
+But be careful. Your goal is to correct at least as possible the provided text. Everytime you correct something, you get a penalty point. 
+You need to minimize your total of penalty points. It's ok if errors remain in the text. The only changes you're allowed to do needs to be in the following guideline.
+
 Correction guideline:
 * If you recognize an ingredient and notice a typo, fix the typo. Otherwise, don't;
 * Line breaks in the package list of ingredients leads to this error: "<subword1>  -  <subword2>". Join them into a single <word>;
 * Some ingredients are enclosed within underscores, such as _milk_ or _cacahuetes_, to denote ingredients that are allergens. Keep them! But if "_" is used for a sequence of words instead, such as "_Trazas de frutos de cáscara_", which is not an ingredient, remove the underscores;
-* If you don't recognize an ingredient, which can happen because of the OCR, and you're not sure about the correct ingredient, keep it as it is;
-* Don't modify the last punctuation of the list;
-* Don't perform uppercase to lowercase changes, and vice-versa, except after a period (.) or for proper names;
-* Don't try to predict percentages if some elements are missing. It would be catastrophic to implement errors. If you're not sure about the correct percentage, keep it as it is.
-* Don't try to over change the provided text. Keep the same structure, words and whitespaces as much as possible. Focus only on the previous cited rules;
+* Punctuation should only be used to separate ingredients in the list of ingredients. If the punctuation is missing between 2 ingredients, add one. 
+* Perform uppercase to lowercase changes, and vice-versa, except after a period (.) or for proper names;
+* Don't try to predict percentages if some elements are missing. It would be catastrophic to implement errors.
+* Keep the same structure, words and whitespaces as much as possible. Focus only on the previous cited rules;
 
 Here's a list of examples:
 ###List of ingredients:
-24, 36 % chocolat noir 63% origine non ue (cacao, scre, beurre de cacao, émulsifiant léci - thine de colza, vanille bourbon gousse), œuf, farine de blé, beurre, sucre, mel, sucre perlé, levure chimique, zeste de citron
+24, 36 % chocolat noir 63% origine non ue (cacao, scre, beurre de cacao, émulsifiant léci - thine de colza, vanille bourbon gousse), œuf, farine de blé, beurre , sucre, mel, sucre perlé,  levure chimique, zeste de citron
 
 ###Corrected list of ingredients:
-24, 36 % chocolat noir 63% origine non ue (cacao, sucre, beurre de cacao, émulsifiant lécithine de colza, vanille bourbon gousse), œuf, farine de blé, beurre, sucre, miel, sucre perlé, levure chimique, zeste de citron
+24, 36 % chocolat noir 63% origine non ue (cacao, sucre, beurre de cacao, émulsifiant lécithine de colza, vanille bourbon gousse), œuf, farine de blé, beurre , sucre, miel, sucre perlé,  levure chimique, zeste de citron
 
 ###List of ingredients:
-GauftcŒeza.fÉ$&ig.e.b- Ingré lents : farin de blé, Comté aop (_lait_) 3 ,5%, sirop de glucose, beurre pâtissier 13%, poudre de lait entier, oeufs entiers, sel, fibre de blé, poivre de Madagascar. Fabriqué dans un atelier qui utilise des fruits à coque.
+Ingré lents : farin de blé, Comté aop (_lait_) 3 ,5%, sirop de glucose beurre pâtissier 13%,poudre de lait entier, oeufs entiers, sel, fibre de blé, poivre de Madagascar. Fabriqué dans un atelier qui utilise des fruits à coque.
 
 ###Corrected list of ingredients:
-Ingrédients : farine de blé, Comté aop (_lait_) 3 ,5%, sirop de glucose, beurre pâtissier 13%, poudre de lait entier, oeufs entiers, sel, fibre de blé, poivre de Madagascar. Fabriqué dans un atelier qui utilise des fruits à coque.
+Ingrédients : farine de blé, Comté aop (_lait_) 3 ,5%, sirop de glucose, beurre pâtissier 13%,poudre de lait entier, oeufs entiers, sel, fibre de blé, poivre de Madagascar. Fabriqué dans un atelier qui utilise des fruits à coque.
 
 ###List of ingredients:
-BASIL (50%), EXTRA VIRGIN OLIVE OIL (32 %), PINE NUTS (4%), Bamboo Fibre, Sugar, Garlic, PECORINO ROMANO PDO CHEESE (196) (Milk), Salt,
+BASIL (50%), EXTRA VIRGIN OLIVE OIL (32 %), PINE NUTS (4%), Bamboo Fibre, Sugar, Garlic,PECORINO ROMANO PDO CHEESE (196) (Milk), Salt,
 
 ###Corrected list of ingredients:
-BASIL (50%), EXTRA VIRGIN OLIVE OIL (32 %), PINE NUTS (4%), Bamboo Fibre, Sugar, Garlic, PECORINO ROMANO PDO CHEESE (196) (Milk), Salt,
+BASIL (50%), EXTRA VIRGIN OLIVE OIL (32 %), PINE NUTS (4%), Bamboo Fibre, Sugar, Garlic,PECORINO ROMANO PDO CHEESE (196) (Milk), Salt,
 
 ###List of ingredients:
-_Cacahuetes_ con cáscara tostado. _Trazas de frutos de cáscara_.
+Κάθετη μονάδα παραγωγής και επεξεργασίας Συστατικά:  Πολτός ελληνικού φυστικιού (70%), υδρογονωμένο φοινικέλαιο, ζάχαρη, καραμέλα (396, κομμάτια ψημένου ελληνικού φυστικιού (2%) , αρωματικές ύλες, αλάτι. Διατηρείται σε δροσερό και σκιερό μέρος. Η παρουσία λαδιού στην επιφάνεια είναι φυσικό φαινόμενο. Ανα κατέψτε καλά Πριν από κάθε χρήση. Παράγεται και συσκευάζεται στην Ελλάδα από : Χρήστος Αγριανίδης. φυστικιού . Αμμουδιά Σερρν,
 
 ###Corrected list of ingredients:
-_Cacahuetes_ con cáscara tostado. Trazas de frutos de cáscara.
-
-###List of ingredients:
-Κάθετη μονάδα παραγωγής και επεξεργασίας Συστατικά: Πολτός ελληνικού φυστικιού (70%), υδρογονωμένο φοινικέλαιο, ζάχαρη, καραμέλα (396, κομμάτια ψημένου ελληνικού φυστικιού (2%) , αρωματικές ύλες, αλάτι. Διατηρείται σε δροσερό και σκιερό μέρος. Η παρουσία λαδιού στην επιφάνεια είναι φυσικό φαινόμενο. Ανα κατέψτε καλά Πριν από κάθε χρήση. Παράγεται και συσκευάζεται στην Ελλάδα από : Χρήστος Αγριανίδης. φυστικιού . Αμμουδιά Σερρν,
-
-###Corrected list of ingredients:
-Κάθετη μονάδα παραγωγής και επεξεργασίας Συστατικά: Πολτός ελληνικού φυστικιού (70%), υδρογονωμένο φοινικέλαιο, ζάχαρη, καραμέλα (396, κομμάτια ψημένου ελληνικού φυστικιού (2%) , αρωματικές ύλες, αλάτι. Διατηρείται σε δροσερό και σκιερό μέρος. Η παρουσία λαδιού στην επιφάνεια είναι φυσικό φαινόμενο. Ανα κατέψτε καλά Πριν από κάθε χρήση. Παράγεται και συσκευάζεται στην Ελλάδα από : Χρήστος Αγριανίδης. φυστικιού . Αμμουδιά Σερρν,
+Κάθετη μονάδα παραγωγής και επεξεργασίας Συστατικά:  Πολτός ελληνικού φυστικιού (70%), υδρογονωμένο φοινικέλαιο, ζάχαρη, καραμέλα (396, κομμάτια ψημένου ελληνικού φυστικιού (2%) , αρωματικές ύλες, αλάτι. Διατηρείται σε δροσερό και σκιερό μέρος. Η παρουσία λαδιού στην επιφάνεια είναι φυσικό φαινόμενο. Ανα κατέψτε καλά Πριν από κάθε χρήση. Παράγεται και συσκευάζεται στην Ελλάδα από : Χρήστος Αγριανίδης. φυστικιού . Αμμουδιά Σερρν,
 """
 
 @dataclass
