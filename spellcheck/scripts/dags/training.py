090cee883c54c89d213f181bde30bbd4f5b130ca
JeremyArancio <jeremyarancio@gmail.com>
2024-05-30 15:09:48 +0200

    feat(spellcheck): :art: Training flan-t5 with Sagemaker

--- spellcheck/scripts/dags/training.py
+++ spellcheck/scripts/dags/training.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+import comet_ml
 from metaflow import FlowSpec, step
 from omegaconf import OmegaConf
 from sagemaker.huggingface import HuggingFace
@@ -14,22 +16,24 @@ CONF_PATH = REPO_DIR / "config/training.yml"
 
 LOGGER = get_logger()
 
+load_dotenv()
+
 
 class TrainingPipeline(FlowSpec):
     """"""
-
     @step
     def start(self):
         self.conf = OmegaConf.load(CONF_PATH)
-        LOGGER.info(f"{self.conf}")
+        LOGGER.info(f"Config: {self.conf}")
         self.next(self.train)
 
     @step
     def train(self):
         """"""
         estimator = HuggingFace(
-            entry_point          = self.conf.estimator.entry_point,           # train script
-            source_dir           = self.conf.estimator.source_dir,            # directory which includes all the files needed for training
+            source_dir           = self.conf.estimator.source_dir,            # directory containing training script and requirements requirements.
+            entry_point          = self.conf.estimator.entry_point,           # train script            
+            dependencies         = self.conf.estimator.dependencies,          # Additional local library
             output_path          = self.conf.estimator.output_path,           # s3 path to save the artifacts
             code_location        = self.conf.estimator.code_location,         # s3 path to stage the code during the training job
             instance_type        = self.conf.estimator.instance_type,         # instances type used for the training job
@@ -42,13 +46,13 @@ class TrainingPipeline(FlowSpec):
             environment          = {
                 "COMET_PROJECT_NAME": os.getenv("COMET_PROJECT_NAME"),
                 "COMET_API_KEY": os.getenv("COMET_API_KEY"),
-                "EXPERIMENT_TAGS": self.conf.estimator.comet_ml_tags
+                "EXPERIMENT_TAGS": self.conf.estimator.comet_ml_tags,
             },                                                                 # environment variables used during training 
             hyperparameters      = self.conf.hyperparameters,                 # the hyperparameters used for the training job
         )
         estimator.fit(wait=True) # Wait for the pipeline
         self.next(self.end)
-    
+
     @step
     def end(self):
         pass
--- spellcheck/scripts/training/flan-t5/flan-t5.py
+++ spellcheck/scripts/training/flan-t5/flan-t5.py
@@ -1,5 +1,6 @@
 """Flan-T5 training script."""
 import os
+import sys
 from typing import Mapping, Iterable, Any, Tuple
 import argparse
 from distutils.util import strtobool
@@ -13,14 +14,16 @@ from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 from datasets import load_dataset, disable_caching
 import numpy as np
 
-# from spellcheck.evaluation.evaluator import SpellcheckEvaluator 
+from spellcheck.evaluation.evaluator import SpellcheckEvaluator 
 
 
 disable_caching()
 
-logger = logging.basicConfig(
-    level=logging.getLevelName(logging.INFO),
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+        level=logging.getLevelName("INFO"),
+        handlers=[logging.StreamHandler(sys.stdout)],  # Get training logging during training job on Sagemaker
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 
@@ -42,9 +45,14 @@ def parse_args():
     parser.add_argument("--warmup_steps", type=int, default=0, help="Number of steps used for a linear warmup from 0 to `learning_rate`")
     parser.add_argument("--fp16", type=strtobool, default=False, help="Whether to use bf16.")
     parser.add_argument("--generation_max_tokens", type=int, default=512, help="Max tokens used for text generation in the Trainer module.")
+    
+    # Versions
+    parser.add_argument("--training_data_version", type=str, help="Training dataset version.")
+    parser.add_argument("--evaluation_data_version", type=str, help="Evaluation dataset version.")
+
     # Evaluation
     parser.add_argument("--beta", type=float, default=1, help="Coefficient used in f1-beta score. beta < 1 favors Precision over Recall.")
-    
+
     args = parser.parse_known_args()
     return args
 
@@ -64,59 +72,26 @@ def copy_files(dir: str, *filenames: Iterable[str]) -> None:
 
 
 class FlanT5Training:
-
+    """Flan-T5 training. 
+    """
     padding = "max_length"                               # Padding configuration. "max_length" means the moodel maxm length
     instruction = "Correct the list of ingredients: "    # Flan-T5 was pretrained using an instruction. We reuse the same
 
     def train(self, args):
+        """Training.
 
+        Args:
+            args: Arguments from argparse.
+        """
         # Load tokenizer and model from HuggingFace Hub
         tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_name)
         model = AutoModelForSeq2SeqLM.from_pretrained(args.pretrained_model_name)
 
         # Load data
-        train_dataset = load_dataset(path=args.training_data, split="train") 
+        train_dataset = load_dataset(path=args.training_data, split="train+test") 
         benchmark_dataset = load_dataset(path=args.evaluation_data, split="train")
 
-        #TODO: Solve dependency issue
-        # # Evaluation
-        # evaluator = SpellcheckEvaluator(benchmark_dataset["original"], beta=args.beta)
-
-        # def compute_metrics(eval_preds: Tuple) -> Mapping[str, Any]:
-        #     """Metrics calculation used by Trainer.
-        #     The function needs to be nested into the training function because of CometML, 
-        #     which loses tracking otherwise.
-
-        #     Args:
-        #         eval_preds (Tuple): Containing prediction and label tokens 
-
-        #     Returns:
-        #         Mapping: Metrics on the evaluation dataset
-        #     """
-        #     # Get the experiment initialized by Trainer
-        #     experiment = comet_ml.get_global_experiment()
-
-        #     pred_tokens, ref_tokens = eval_preds
-
-        #     # Need to convert -100 tokens back to pad_token
-        #     ref_tokens = np.where(ref_tokens != -100, ref_tokens, tokenizer.pad_token_id)
-        #     pred_tokens = np.where(pred_tokens != -100, pred_tokens, tokenizer.pad_token_id)
-            
-        #     # Transform back in texts
-        #     predictions = tokenizer.batch_decode(pred_tokens, skip_special_tokens=True)
-        #     references = tokenizer.batch_decode(ref_tokens, skip_special_tokens=True)
-            
-        #     # Usage of our custom evaluation algorithm
-        #     metrics = evaluator.evaluate(predictions=predictions, references=references)
-        #     logging.info(f"Metrics: {metrics}")
-
-        #     if experiment:
-        #         experiment.log_metrics(metrics)
-        #     else:
-        #         logging.warning("No experiment in compute_metrics.")
-        #     return metrics
-
-        # Prepare data for training
+        # Prepare datasets for training
         preprocessed_train_dataset = train_dataset.map(
             self.preprocess, 
             batched=True, 
@@ -137,6 +112,49 @@ class FlanT5Training:
                 "tokenizer": tokenizer,
             }
         )
+
+        # Custom evaluation
+        evaluator = SpellcheckEvaluator(
+            originals=benchmark_dataset["original"], 
+            beta=args.beta
+        )
+
+        def compute_metrics(eval_preds: Tuple) -> Mapping[str, Any]:
+            """Metrics calculation used by Trainer.
+            The function needs to be nested into the training function because of CometML, 
+            which loses tracking otherwise.
+
+            Args:
+                eval_preds (Tuple): Containing prediction and label tokens 
+
+            Returns:
+                Mapping: Metrics on the evaluation dataset
+            """
+            # Get the experiment initialized by Trainer
+            experiment = comet_ml.get_global_experiment()
+
+            pred_tokens, ref_tokens = eval_preds
+
+            # Need to convert -100 tokens back to pad_token
+            ref_tokens = np.where(ref_tokens != -100, ref_tokens, tokenizer.pad_token_id)
+            pred_tokens = np.where(pred_tokens != -100, pred_tokens, tokenizer.pad_token_id)
+            
+            # Transform back in texts
+            predictions = tokenizer.batch_decode(pred_tokens, skip_special_tokens=True)
+            references = tokenizer.batch_decode(ref_tokens, skip_special_tokens=True)
+
+            # Custom evaluation
+            metrics = evaluator.evaluate(predictions=predictions, references=references)
+            LOGGER.info(f"Metrics: {metrics}")
+
+            if experiment:
+                experiment.log_metrics(metrics)
+                # Log texts
+                experiment.log_text(references[10], metadata={"type": "Reference"})
+                experiment.log_text(predictions[10], metadata={"type": "Prediction"})
+            else:
+                logging.warning("No experiment in compute_metrics.")
+            return metrics
 
         # Ignore tokenizer pad_token in the loss compuation
         label_pad_token_id = -100
@@ -168,6 +186,7 @@ class FlanT5Training:
             save_total_limit                    = 2,                                   # Number checkpoints saved at the same
             load_best_model_at_end              = True,
             # metric_for_best_model               = "f1_beta",                           # Metric used to select the best model.
+            report_to="comet_ml"
         )
         trainer = Seq2SeqTrainer(
             model           = model,
@@ -175,10 +194,48 @@ class FlanT5Training:
             data_collator   = data_collator,
             train_dataset   = preprocessed_train_dataset,
             eval_dataset    = preprocessed_benchmark_dataset,
-            # compute_metrics = compute_metrics,
+            compute_metrics = compute_metrics,
         )
         trainer.train()
+        LOGGER.info("Training finished.")
+        
+        # Finish Experiment tracking logging
+        LOGGER.info("Start logging additional info into the experiment tracker.")
+        # This process is required since the a bug with CometML shuts down connection to the experiment run
+        experiment = comet_ml.get_global_experiment()
+        LOGGER.info(f"Experiment name after Transformers trainer: {experiment.get_name()}")
+        experiment = comet_ml.ExistingExperiment(experiment_key=experiment.get_key())
+        
+        # Experiment tags
+        tags = os.getenv("EXPERIMENT_TAGS") if isinstance(os.getenv("EXPERIMENT_TAGS"), list) else [os.getenv("EXPERIMENT_TAGS")]
+        LOGGER.info(f"Log tags: {tags}")
+        experiment.add_tags(tags)
 
+        # Artifacts
+        LOGGER.info("Start logging artifacts.")
+        training_dataset_artifact = comet_ml.Artifact(
+            name="Spellcheck-training-dataset",
+            artifact_type="dataset",
+            version_tags=args.training_data_version,
+        )
+        evaluation_dataset_artifact = comet_ml.Artifact(
+            name="Spellcheck-evaluation_dataset",
+            artifact_type="dataset",
+            version_tags=args.evaluation_data_version,
+        )
+        training_dataset_artifact.add_remote(
+            uri=args.training_data,
+            sync_mode=False
+        )
+        evaluation_dataset_artifact.add_remote(
+            uri=args.evaluation_data,
+            sync_mode=False
+        )
+        experiment.log_artifact(training_dataset_artifact)
+        experiment.log_artifact(evaluation_dataset_artifact)
+        LOGGER.info("Artifacts logged.")
+        
+        LOGGER.info("Training job finished.")
 
     def preprocess(
             self, 
