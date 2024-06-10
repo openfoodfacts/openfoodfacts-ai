1ecc4f8a467a0b0b15a2a9852308dd0272fc8ada
JeremyArancio <jeremyarancio@gmail.com>
2024-06-10 12:43:15 +0200

    test(spellcheck): :construction: Add dotenv for local testing of the training script

--- spellcheck/scripts/training/flan-t5/flan-t5.py
+++ spellcheck/scripts/training/flan-t5/flan-t5.py
@@ -2,21 +2,23 @@
 import os
 import sys
 import json
-from typing import Mapping, Iterable, Any, Tuple
+from typing import Mapping, Iterable
 import argparse
 from distutils.util import strtobool
 import shutil
 import logging
+from dotenv import load_dotenv
 
 import comet_ml
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import DataCollatorForSeq2Seq
 from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 from datasets import load_dataset, disable_caching
-import numpy as np
 
 from spellcheck.evaluation.evaluator import SpellcheckEvaluator 
 
+# For testing
+load_dotenv()
 
 disable_caching()
 
@@ -62,7 +64,7 @@ def parse_args():
     parser.add_argument("--optim", type=str, default="adamw_torch", help="Optimizer.")
     parser.add_argument("--lr_scheduler_type", type=str, default="linear", help="Learning scheduler type.")
     parser.add_argument("--gradient_accumulation_steps", type=int, default=0, help="Accumulate bacthes before back propagation.")
-    parser.add_argument("--instruction", type=str, default="", help="Flan-T5 instruction.")
+    parser.add_argument("--instruction", type=str, default="Correct: ", help="Flan-T5 instruction.")
 
     # Versions
     parser.add_argument("--training_data_version", type=str, help="Training dataset version.")
@@ -103,22 +105,25 @@ class FlanT5Training:
         """
         # Load tokenizer and model from HuggingFace Hub
         tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_name)
-        model = AutoModelForSeq2SeqLM.from_pretrained(args.pretrained_model_name)
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            args.pretrained_model_name,
+            use_cache= False if args.gradient_checkpointing else True,
+        )
 
         # Load data
-        train_dataset = load_dataset(path=args.training_data, split="train+test") 
+        train_dataset = load_dataset(path=args.training_data) 
         evaluation_dataset = load_dataset(path=args.evaluation_data, split="train")
         LOGGER.info(f"Training dataset: {train_dataset}")
         LOGGER.info(f"Evaluation dataset: {evaluation_dataset}")
         
         # Add instruction for Flan-T5
-        self.instruction = args.instruction
+        self.instruction = "" if args.instruction == "none" else args.instruction
 
         # Prepare datasets for training
         preprocessed_train_dataset = train_dataset.map(
             self.preprocess, 
             batched=True, 
-            remove_columns=train_dataset.column_names, 
+            remove_columns=train_dataset["train"].column_names, 
             fn_kwargs={
                 "input_name": "text", 
                 "target_name": "label",
@@ -136,50 +141,6 @@ class FlanT5Training:
             }
         )
 
-        # Custom evaluation
-        evaluator = SpellcheckEvaluator(
-            originals=evaluation_dataset["original"], 
-            beta=args.beta
-        )
-
-        def compute_metrics(eval_preds: Tuple) -> Mapping[str, Any]:
-            """Metrics calculation used by Trainer.
-            The function needs to be nested into the training function because of CometML, 
-            which loses tracking otherwise.
-
-            Args:
-                eval_preds (Tuple): Containing prediction and label tokens 
-
-            Returns:
-                Mapping: Metrics on the evaluation dataset
-            """
-            # Get the experiment initialized by Trainer
-            experiment = comet_ml.get_global_experiment()
-
-            pred_tokens, ref_tokens = eval_preds
-
-            # Need to convert -100 tokens back to pad_token
-            ref_tokens = np.where(ref_tokens != -100, ref_tokens, tokenizer.pad_token_id)
-            
-            # Transform back in texts
-            predictions = tokenizer.batch_decode(pred_tokens, skip_special_tokens=True)
-            references = tokenizer.batch_decode(ref_tokens, skip_special_tokens=True)
-            LOGGER.info(f"First prediction element: {predictions[0]}\nFirst reference element: {references[0]}")
-
-            # Custom evaluation
-            metrics = evaluator.evaluate(predictions=predictions, references=references)
-            LOGGER.info(f"Metrics: {metrics}")
-
-            if experiment:
-                experiment.log_metrics(metrics)
-                # Log texts
-                for idx in [5, 50, 100]:
-                    experiment.log_text(predictions[idx], metadata={"type": "Prediction"})
-                    experiment.log_text(references[idx], metadata={"type": "Reference"})
-            else:
-                logging.warning("No experiment in compute_metrics.")
-            return metrics
-
         # Ignore tokenizer pad_token in the loss compuation
         label_pad_token_id = -100
         # Data collator
@@ -190,10 +151,6 @@ class FlanT5Training:
             pad_to_multiple_of=8
         )
 
-        # Gradient checkpointing
-        if args.gradient_checkpointing:
-            model.use_cache = False
-        
         # Training
         training_args = Seq2SeqTrainingArguments(
             output_dir                          = args.output_dir,                     # Model checkpoints directory
@@ -210,15 +167,12 @@ class FlanT5Training:
             gradient_checkpointing              = args.gradient_checkpointing,
             optim                               = args.optim,                          # AdamW or AdaFactor        
             lr_scheduler_type                   = args.lr_scheduler_type,
-            gradient_accumulation_steps         = args.gradient_accumulation_steps,           
+            gradient_accumulation_steps         = args.gradient_accumulation_steps,         
             #Logging & evaluation strategies
             logging_dir                         = f"{args.output_dir}/logs",
             logging_strategy                    = "steps",
             logging_steps                       = 100,
             evaluation_strategy                 = "epoch",
-            save_strategy                       = "epoch",
-            save_total_limit                    = 2,                                   # Number checkpoints saved at the same
-            load_best_model_at_end              = True,
             # metric_for_best_model               = "f1_beta",                           # Metric used to select the best model.
             report_to="comet_ml",
         )
@@ -226,22 +180,48 @@ class FlanT5Training:
             model           = model,
             args            = training_args,
             data_collator   = data_collator,
-            train_dataset   = preprocessed_train_dataset,
-            eval_dataset    = preprocessed_evaluation_dataset,
-            compute_metrics = compute_metrics,
+            train_dataset   = preprocessed_train_dataset["train"],
+            eval_dataset    = preprocessed_train_dataset["test"],
         )
         trainer.train()
         LOGGER.info("Training finished.")
 
-        # Run and upload benchmark predictions to S3
-        LOGGER.info("Start exporting benchmark predictions to S3.")
-        predictions, _, _ = trainer.predict(preprocessed_evaluation_dataset)
-        preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-        prediction_dataset = evaluation_dataset.add_column(name="prediction", column=preds)
+        # Save best model and tokenizer
+        tokenizer.save_pretrained(args.output_dir)
+        trainer.save_model(args.output_dir)
+
+        # Finish Experiment tracking logging
+        LOGGER.info("Start logging additional info into the experiment tracker.")
+
+        # This process is required since the a bug with CometML shuts down connection to the experiment run
+        experiment = comet_ml.get_global_experiment()
+        LOGGER.info(f"Experiment name after Transformers trainer: {experiment.get_name()}")
+        experiment = comet_ml.ExistingExperiment(experiment_key=experiment.get_key())
+
+        # Run, evaluate and upload benchmark predictions to S3
+        evaluator = SpellcheckEvaluator(originals=evaluation_dataset["original"], beta=args.beta)
+        LOGGER.info("Start evaluating model on benchmark.")
+        preds, _, _ = trainer.predict(preprocessed_evaluation_dataset)
+        predictions = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        metrics = evaluator.evaluate(predictions=predictions, references=evaluation_dataset["reference"])
+        LOGGER.info(f"Evaluation metrics: {metrics}")
+        experiment.log_metrics(metrics)
+        # Upload predictions to S3
+        prediction_dataset = evaluation_dataset.add_column(name="prediction", column=predictions)
         s3_evaluation_path = os.path.join(os.getenv("S3_EVALUATION_URI"), "evaluation-" + SM_JOB_NAME)
         LOGGER.info(f"S3 URI where predictions on evaluation are sent to: {s3_evaluation_path}")
         prediction_dataset.save_to_disk(s3_evaluation_path)
 
+
+        # Finish Experiment tracking logging
+        LOGGER.info("Start logging additional info into the experiment tracker.")
+
+        # This process is required since the a bug with CometML shuts down connection to the experiment run
+        experiment = comet_ml.get_global_experiment()
+        LOGGER.info(f"Experiment name after Transformers trainer: {experiment.get_name()}")
+        experiment = comet_ml.ExistingExperiment(experiment_key=experiment.get_key())
+        
+        
         # Finish Experiment tracking logging
         LOGGER.info("Start logging additional info into the experiment tracker.")
 
@@ -296,11 +276,7 @@ class FlanT5Training:
         # add prefix to the input for t5
         inputs = [self.instruction + item for item in sample[input_name]]
         # tokenize inputs
-        model_inputs = tokenizer(
-            inputs, 
-            padding=self.padding, 
-            truncation=True
-        )
+        model_inputs = tokenizer(inputs, padding=self.padding, truncation=True)
         # Tokenize targets
         labels = tokenizer(sample[target_name], padding=self.padding, truncation=True)
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
