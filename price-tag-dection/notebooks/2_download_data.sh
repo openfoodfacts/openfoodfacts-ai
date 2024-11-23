f6504a2d282e4d9aaad8ab6b880c41f1b1dc3b0b
Basli, Adel <Adel.Basli@nielseniq.com>
2024-11-23 08:08:35 +0100

    clean files and fix label error

--- price-tag-dection/notebooks/2_download_data.sh
+++ price-tag-dection/notebooks/2_download_data.sh
@@ -13,7 +13,7 @@ fi
 
 alias ml-cli='${CONDA_PREFIX}/bin/python3 ${project_path}/main.py'
 # Export the data using the command line
-ml-cli datasets export --project-id ${project_id} --from ls --to ultralytics --output-dir ${data_path} --label-names 'price-tag' --api-key ${LABEL_STUDIO_KEY}
+ml-cli datasets export --project-id ${project_id} --from ls --to ultralytics --output-dir ${data_path} --label-names 'product,price-tag' --api-key ${LABEL_STUDIO_KEY}
 
 # Update the path data in data.yaml
 sed -i '' "s|path: data|path: ${data_path}/data|1" "${data_path}/data.yaml"
--- price-tag-dection/notebooks/3_run_yolo_model.py
+++ price-tag-dection/notebooks/3_run_yolo_model.py
@@ -1,7 +1,7 @@
 #%%
 from ultralytics import YOLO
 #%%
-data_path =  "/Users/baslad01/data_dump/openfoodfacts/yolo"
+data_path = "/Users/baslad01/data_dump/openfoodfacts/yolo"
 
 # Load a model
 model = YOLO(f"{data_path}/yolo11n.pt")
@@ -10,10 +10,10 @@ model = YOLO(f"{data_path}/yolo11n.pt")
 train_results = model.train(
     data=f"{data_path}/data.yaml",  # path to dataset YAML
     epochs=1000,  # number of training epochs
-    patience=50,  # early stopping patience
+    patience=20,  # early stopping patience
     dropout=0.1,  # dropout probability
     imgsz=640,  # training image size
-    plots = True,  # create plots
+    plots=True,  # create plots
     device="mps",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
     project=f"{data_path}/model",  # save training results to project/name
 )
