d709ea220d34d9daf04a10ea76863d13f5041157
Basli, Adel <Adel.Basli@nielseniq.com>
2024-11-27 09:03:35 +0100

    increase patience as it seems to keep learning

--- price-tag-dection/notebooks/3_run_yolo_model.py
+++ price-tag-dection/notebooks/3_run_yolo_model.py
@@ -16,7 +16,7 @@ print(f"CPU count: {cpu_count}")
 train_results = model.train(
     data=f"{data_path}/data.yaml",  # path to dataset YAML
     epochs=1000,  # number of training epochs
-    patience=20,  # early stopping patience
+    patience=30,  # early stopping patience
     dropout=0.1,  # dropout probability
     imgsz=640,  # training image size
     plots=True,  # create plots
