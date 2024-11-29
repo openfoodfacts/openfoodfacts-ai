52a123001ed953166df8cf9262dfe49bc0657ae7
Basli, Adel <Adel.Basli@nielseniq.com>
2024-11-29 15:41:06 +0100

    add missing package for linux

--- price-tag-dection/notebooks/3_run_yolo_model.py
+++ price-tag-dection/notebooks/3_run_yolo_model.py
@@ -17,6 +17,7 @@ train_results = model.train(
     data=f"{data_path}/data.yaml",  # path to dataset YAML
     epochs=1000,  # number of training epochs
     patience=30,  # early stopping patience
+    batch=-1,  # batch size
     dropout=0.1,  # dropout probability
     imgsz=640,  # training image size
     plots=True,  # create plots
