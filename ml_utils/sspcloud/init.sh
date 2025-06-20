f477224785ae9f7ae6411cb8fc45be1c84d67494
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2025-06-20 10:00:04 +0200

    fix init script for SSPCloud
    - disable unattended upgrades, as it made the script failed due to dpkg
      lock
    - install uv

--- ml_utils/sspcloud/init.sh
+++ ml_utils/sspcloud/init.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 # Bash init script for SSP Cloud (https://datalab.sspcloud.fr/), used
 # to set up the environment for a user.
 
-# We save all logs in log.out to debug
+# We save all logs in log.out for debugging
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 
-# Same for envvar
+# Disable unattended upgrades to avoid lock issues
+sudo systemctl stop unattended-upgrades
+
+# We save envvar to file for debugging
 env | sort > env_init.out
 
 # And for the used init script
@@ -35,6 +41,15 @@ git clone --depth=1 https://github.com/openfoodfacts/openfoodfacts-ai.git
 export folder="/home/onyxia/work/openfoodfacts-ai"
 
 sudo -u ${USERNAME} sed -i "s/cd \/home\/onyxia\/work/cd \/home\/onyxia\/work\/openfoodfacts-ai/" /home/onyxia/.bashrc
+
+# Install uv
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Add ~/.local/bin to PATH in .bashrc
+if ! grep -q 'export PATH="$HOME/.local/bin:$PATH"' /home/onyxia/.bashrc; then
+  echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/onyxia/.bashrc
+fi
 
 # Final env (for debugging)
 env | sort > env_final.out
